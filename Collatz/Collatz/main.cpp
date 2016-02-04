ba7156171bdd64629ee39f5ec10382bff6f2d1ed
almto3 <almto3@hotmail.com>
2016-02-04 14:01:46 -0600

    Getting user input

--- Collatz/Collatz/main.cpp
+++ Collatz/Collatz/main.cpp
@@ -7,9 +7,29 @@
 //
 
 #include <iostream>
+#include <sstream>
+using namespace std;
+
+int getinput(){
+    int x = 0;
+    string input = "";
+    
+    while (true) {
+        cout << "Please enter a valid number: ";
+        getline(cin, input);
+        
+        // This code converts from string to number safely.
+        stringstream myStream(input);
+        if (myStream >> x)
+            break;
+        cout << "Invalid number, please try again" << endl;
+    }
+    cout << "You entered: " << x << endl << endl;
+    return x;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int x = getinput();
+    cout << x;
     return 1;
 }
