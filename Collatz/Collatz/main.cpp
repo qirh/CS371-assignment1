0f55b0c29bec7bbfb2c28e6934b953465dc28fc9
almto3 <almto3@hotmail.com>
2016-02-04 13:36:57 -0600

    first commit (just basic code)

---
+++ Collatz/Collatz/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Collatz
+//
+//  Created by saleh alghusson on 2/4/16.
+//  Copyright © 2016 saleh alghusson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
